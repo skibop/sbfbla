442824fcbc05856cc39e27fddf13b62438c23454
Ankit Kale <kaleankit7979@outlook.com>
2025-02-05 13:19:57 -0500

    changes to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
   }, [isOpen])
 
   return (
-    <nav className="fixed w-full z-10 bg-blue-900 bg-opacity-75">
+    <nav className="fixed w-full z-10 bg-gray-900 bg-opacity-75">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="hidden md:flex items-center space-x-4">
